cadc272d22b36e32bc04d67f3d9164b9f3c3ff59
x <tmkgdgfhxlfhzrwphchjxxs@gmail.com>
2023-04-27 05:22:43 +0000

    .

--- src/users/dtos/inputs/update_me.body.ts
+++ src/users/dtos/inputs/update_me.body.ts
@@ -1,10 +1,12 @@
-import { MaxLength, MinLength } from 'class-validator';
+import { MaxLength, MinLength, IsOptional } from 'class-validator';
 
 export class UpdateMeBody {
+  @IsOptional()
   @MinLength(4)
   @MaxLength(20)
   name?: string;
 
+  @IsOptional()
   @MinLength(4)
   @MaxLength(100)
   password?: string;
