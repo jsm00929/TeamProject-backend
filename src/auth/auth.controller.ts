03a4eddaf07ea269e096fa6b774970d640ac76cb
x <xxx@xxx.xx>
2023-05-02 16:23:05 +0900

    fix: redirect to client url when failed to login or signup with google

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,103 +1,104 @@
-import { Request, Response } from 'express';
-import { RequestWith } from '../core/types/request_with';
-import { SignupBody } from './dtos/inputs/signup.body';
+import {Request, Response} from 'express';
+import {RequestWith} from '../core/types/request_with';
+import {SignupBody} from './dtos/inputs/signup.body';
 import authService from './auth.service';
-import { HttpStatus } from '../core/constants/http_status';
-import { LoginBody } from './dtos/inputs/login.body';
-import {
-  clearAuthCookies,
-  setAccessTokenCookie,
-  setAuthCookies,
-} from '../utils/cookie_store';
-import {
-  GOOGLE_LOGIN_OAUTH2_URL,
-  GOOGLE_SIGNUP_OAUTH2_URL,
-  REFRESH_TOKEN_COOKIE_NAME,
-} from '../config/constants';
-import { verifyRefreshToken } from '../utils/token';
-import { AppResult } from '../core/types/app_result';
-import { AppError } from '../core/types';
-import { ErrorMessages } from '../core/constants';
-import { GoogleLoginCodeQuery } from './dtos/inputs/google_login_code.query';
-import { Config } from '../config/env';
+import {HttpStatus} from '../core/constants/http_status';
+import {LoginBody} from './dtos/inputs/login.body';
+import {clearAuthCookies, setAccessTokenCookie, setAuthCookies,} from '../utils/cookie_store';
+import {GOOGLE_LOGIN_OAUTH2_URL, GOOGLE_SIGNUP_OAUTH2_URL, REFRESH_TOKEN_COOKIE_NAME,} from '../config/constants';
+import {verifyRefreshToken} from '../utils/token';
+import {AppResult} from '../core/types/app_result';
+import {AppError} from '../core/types';
+import {ErrorMessages} from '../core/constants';
+import {GoogleLoginCodeQuery} from './dtos/inputs/google_login_code.query';
+import {Config} from '../config/env';
 
-const { clientHost, clientPort } = Config.env;
+const {clientHost, clientPort} = Config.env;
 
 async function signup(req: RequestWith<SignupBody>, res: Response) {
-  const signupInput = req.unwrap();
-  const userId = await authService.signUp(signupInput);
+    const signupInput = req.unwrap();
+    const userId = await authService.signUp(signupInput);
 
-  setAuthCookies(userId, res);
+    setAuthCookies(userId, res);
 
-  return AppResult.new({
-    status: HttpStatus.CREATED,
-  });
+    return AppResult.new({
+        status: HttpStatus.CREATED,
+    });
 }
 
 async function login(req: RequestWith<LoginBody>, res: Response) {
-  const loginInput = req.unwrap();
-  const userId = await authService.login(loginInput);
+    const loginInput = req.unwrap();
+    const userId = await authService.login(loginInput);
 
-  setAuthCookies(userId, res);
+    setAuthCookies(userId, res);
 }
 
 async function logout(_, res: Response) {
-  clearAuthCookies(res);
+    clearAuthCookies(res);
 }
 
 async function refreshToken(req: Request, res: Response) {
-  const token = req.signedCookies[REFRESH_TOKEN_COOKIE_NAME];
-  if (typeof token !== 'string') {
-    throw AppError.new({
-      message: ErrorMessages.INVALID_TOKEN,
-      status: HttpStatus.UNAUTHORIZED,
-    });
-  }
-  const userId = verifyRefreshToken(token);
+    const token = req.signedCookies[REFRESH_TOKEN_COOKIE_NAME];
+    if (typeof token !== 'string') {
+        throw AppError.new({
+            message: ErrorMessages.INVALID_TOKEN,
+            status: HttpStatus.UNAUTHORIZED,
+        });
+    }
+    const userId = verifyRefreshToken(token);
 
-  setAccessTokenCookie(userId, res);
+    setAccessTokenCookie(userId, res);
 }
 
 async function googleSignup(req: Request, res: Response) {
-  res.redirect(GOOGLE_SIGNUP_OAUTH2_URL);
+    res.redirect(GOOGLE_SIGNUP_OAUTH2_URL);
 }
 
 async function googleLogin(req: Request, res: Response) {
-  res.redirect(GOOGLE_LOGIN_OAUTH2_URL);
+    res.redirect(GOOGLE_LOGIN_OAUTH2_URL);
 }
 
-// TODO: unimplemented
 async function googleLoginRedirect(
-  req: RequestWith<GoogleLoginCodeQuery>,
-  res: Response,
+    req: RequestWith<GoogleLoginCodeQuery>,
+    res: Response,
 ) {
-  const { code } = req.unwrap();
-  const userId = await authService.googleLoginRedirect(code);
+    const {code} = req.unwrap();
+    try {
+        const userId = await authService.googleLoginRedirect(code);
 
-  setAuthCookies(userId, res);
-  // return AppResult.redirect(`${clientHost}:${clientPort}`);
-  return AppResult.redirect(`http://${clientHost}:${clientPort}/`);
+        setAuthCookies(userId, res);
+        // return AppResult.redirect(`${clientHost}:${clientPort}`);
+    } catch (e) {
+        // TODO: 구글 로그인 실패 시, REDIRECT URL
+        return AppResult.redirect(`http://${clientHost}:${clientPort}/`);
+    }
+    // 성공시
+    return AppResult.redirect(`http://${clientHost}:${clientPort}/`);
 }
 
 async function googleSignupRedirect(
-  req: RequestWith<GoogleLoginCodeQuery>,
-  res: Response,
+    req: RequestWith<GoogleLoginCodeQuery>,
+    res: Response,
 ) {
-  const { code } = req.unwrap();
-  const userId = await authService.googleSignupRedirect(code);
-
-  setAuthCookies(userId, res);
-  // return AppResult.redirect(`${clientHost}:${clientPort}`);
-  return AppResult.redirect(`http://${clientHost}:${clientPort}/`);
+    const {code} = req.unwrap();
+    try {
+        const userId = await authService.googleSignupRedirect(code);
+        setAuthCookies(userId, res);
+    } catch (e) {
+        // TODO: 구글 로그인 실패 시, REDIRECT URL
+        return AppResult.redirect(`http://${clientHost}:${clientPort}/`);
+    }
+    // 성공시
+    return AppResult.redirect(`http://${clientHost}:${clientPort}/`);
 }
 
 export default {
-  signup,
-  login,
-  logout,
-  refreshToken,
-  googleLogin,
-  googleLoginRedirect,
-  googleSignup,
-  googleSignupRedirect,
+    signup,
+    login,
+    logout,
+    refreshToken,
+    googleLogin,
+    googleLoginRedirect,
+    googleSignup,
+    googleSignupRedirect,
 };
